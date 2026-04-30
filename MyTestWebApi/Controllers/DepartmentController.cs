0605b582cab39c6a72750a227a90c4f1a09098a2
Jahangir Ali <jehangir.ali@gmail.com>
2026-04-30 07:37:05 -0700

    Department

---
+++ MyTestWebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet(Name = "GetDepartment")]
+        public string Get()
+        {
+            return "My Department!";
+        }
+    }
+}
