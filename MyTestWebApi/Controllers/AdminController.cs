9feda076600f46295d9d2b29305d44253da05bd6
Jahangir Ali <jehangir.ali@gmail.com>
2026-04-30 04:29:28 -0700

    Initial commit - .NET API with Docker support

---
+++ MyTestWebApi/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetMyName")]
+        public string Get()
+        {
+            return "Jahangir Ali!";
+        }
+    }
+}
