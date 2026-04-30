402103c4eb004946cecdb472ce72005733c233ad
Jahangir Ali <jehangir.ali@gmail.com>
2026-04-30 06:13:16 -0700

    Dashboard

---
+++ MyTestWebApi/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet(Name = "GetDashboard")]
+        public string Get()
+        {
+            return "My Dashboard!";
+        }
+    }
+}
